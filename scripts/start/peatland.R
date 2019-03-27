c7f5ecc0eae49f89a8e7b79162ba6eee14859e59
florianh <humpenoeder@pik-potsdam.de>
2019-03-27 16:08:45 +0100

    bugfix

--- scripts/start/peatland.R
+++ scripts/start/peatland.R
@@ -30,7 +30,7 @@ source("config/default.cfg")
 cfg$results_folder <- "output/:title:"
 
 #cfg$input <- c(cfg$input,"peatland_input_v1.tgz")
-cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"scp://cluster.pik-potsdam.de/p/projects/landuse/users/florianh/data"=NULL),
+cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"/p/projects/landuse/users/florianh/data"=NULL),
                            getOption("magpie_repos"))
 
 cfg$gms$peatland  <- "ipcc_2014_mar19"
