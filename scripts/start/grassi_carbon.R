d52052273cbdd21253341cee1a81d6329a94397d
florianh <humpenoeder@pik-potsdam.de>
2019-07-06 22:41:14 +0200

    land conversion costs

--- scripts/start/grassi_carbon.R
+++ scripts/start/grassi_carbon.R
@@ -43,7 +43,7 @@ cfg$recalibrate <- TRUE
 for (climatemodel in c("HadGEM2_ES")) {
   #SSP2 Ref CC
   #cfg$title <- paste0("SSP2_RCP60_LPJmL5_HadGEM_v2")
-  cfg$title <- "calib_run_01"
+  cfg$title <- "calib_run_02"
   cfg$input[1] <- paste0("LPJmL5_LAI-",climatemodel,"-rcp6p0_rev34_c200_690d3718e151be1b450b394c1064b1c5.tgz")
   #cfg$input[1] <- paste0("isimip_rcp-",climatemodel,"-rcp6p0-co2_rev34_c200_690d3718e151be1b450b394c1064b1c5.tgz")
   cfg <- setScenario(cfg,c("SSP2","NPI"))
