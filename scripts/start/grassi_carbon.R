7098b94f087c493b2fadbb50b773c5d80a5854ee
florianh <humpenoeder@pik-potsdam.de>
2019-07-08 11:34:48 +0200

    land conversion costs 12000

--- scripts/start/grassi_carbon.R
+++ scripts/start/grassi_carbon.R
@@ -26,9 +26,10 @@ cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp8p5-co2_rev34_c200_690d3718e151be1b45
                "rev4.18_690d3718e151be1b450b394c1064b1c5_magpie.tgz",
                "rev4.18_690d3718e151be1b450b394c1064b1c5_validation.tgz",
                "additional_data_rev3.67.tgz",
-               "calibration_H12_c200_12Sep18.tgz")
+#               "calibration_H12_c200_12Sep18.tgz")
 #               "calibration_H12_c200_LPJmL5_LAI_IPSL_20Jun19.tgz",
-#                "calibration_H12_c200_LPJmL5_LAI_HadGEM_23Jun19.tgz")
+#               "calibration_H12_c200_LPJmL5_LAI_HadGEM_23Jun19.tgz")
+                "calibration_H12_c200_LPJmL5_LAI_HadGEM_06Jul19.tgz")
 
 #turn on CC
 cfg$gms$c14_yields_scenario  <- "cc"
@@ -42,8 +43,8 @@ cfg$recalibrate <- FALSE
 #"NorESM1_M","GFDL_ESM2M","MIROC_ESM_CHEM","HadGEM2_ES",IPSL_CM5A_LR
 for (climatemodel in c("HadGEM2_ES")) {
   #SSP2 Ref CC
-  #cfg$title <- paste0("SSP2_RCP60_LPJmL5_HadGEM_v2")
-  cfg$title <- "calib_run_03"
+  cfg$title <- paste0("SSP2_RCP60_LPJmL5_HadGEM_v3")
+  #cfg$title <- "calib_run_03"
   cfg$input[1] <- paste0("LPJmL5_LAI-",climatemodel,"-rcp6p0_rev34_c200_690d3718e151be1b450b394c1064b1c5.tgz")
   #cfg$input[1] <- paste0("isimip_rcp-",climatemodel,"-rcp6p0-co2_rev34_c200_690d3718e151be1b450b394c1064b1c5.tgz")
   cfg <- setScenario(cfg,c("SSP2","NPI"))
@@ -52,12 +53,12 @@ for (climatemodel in c("HadGEM2_ES")) {
   start_run(cfg,codeCheck=FALSE)
   
   ##SSP2 26 CC
-  cfg$title <- paste0("SSP2_RCP26_LPJmL5_HadGEM_v2")
+  cfg$title <- paste0("SSP2_RCP26_LPJmL5_HadGEM_v3")
   cfg$input[1] <- paste0("LPJmL5_LAI-",climatemodel,"-rcp2p6_rev34_c200_690d3718e151be1b450b394c1064b1c5.tgz")
   #cfg$input[1] <- paste0("isimip_rcp-",climatemodel,"-rcp2p6-co2_rev34_c200_690d3718e151be1b450b394c1064b1c5.tgz")
   cfg <- setScenario(cfg,c("SSP2","NDC"))
   cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg950"
   cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-Budg950"
-  #start_run(cfg,codeCheck=FALSE)
+  start_run(cfg,codeCheck=FALSE)
 }
   
