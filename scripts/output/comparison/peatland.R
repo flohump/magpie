b17783ee430fde9ac172fc3ad680cc313dc3b670
florianh <humpenoeder@pik-potsdam.de>
2019-09-18 17:16:09 +0200

    added start and output scripts

---
+++ scripts/output/comparison/peatland.R
@@ -0,0 +1,202 @@
+# |  (C) 2008-2018 Potsdam Institute for Climate Impact Research (PIK),
+# |  authors, and contributors see AUTHORS file
+# |  This file is part of MAgPIE and licensed under GNU AGPL Version 3
+# |  or later. See LICENSE file or go to http://www.gnu.org/licenses/
+# |  Contact: magpie@pik-potsdam.de
+
+#########################
+#### check modelstat ####
+#########################
+# Version 1.0, Florian Humpenoeder
+#
+library(lucode)
+library(magpie4)
+library(luscale)
+
+options(error=function()traceback(2))
+
+############################# BASIC CONFIGURATION #############################
+if(!exists("source_include")) {
+  outputdirs <- path("output/",list.dirs("output/", full.names = FALSE, recursive = FALSE))
+  #Define arguments that can be read from command line
+  readArgs("outputdirs")
+}
+###############################################################################
+cat("\nStarting output generation\n")
+
+x <- list()
+x$emis_co2_clim_annual <- NULL
+x$emis_all_glo_annual <- NULL
+x$emis_p_glo <- NULL
+x$area_pman_clim_detail <- NULL
+x$area_p_clim <- NULL
+x$area_p_cell <- NULL
+x$area_p_map <- NULL
+x$area_p_map_ratio <- NULL
+x$land_clim <- NULL
+x$fprice <- NULL
+x$fexpshare <- NULL
+x$kcal <- NULL
+x$tau <- NULL
+
+missing <- NULL
+
+calcEmisCum <- function(a) {
+  im_years <- getYears(a,as.integer = T)
+  im_years <- c(0,diff(im_years))
+  im_years <- as.magpie(im_years,temporal=1)
+  getYears(im_years) <- getYears(a)
+  a[,im_years[1],] <- 0
+  a <- a*im_years[,getYears(a),]
+  a <- as.magpie(apply(a,c(1,3),cumsum),spatial=2,temporal=1)
+  return(a)
+}
+
+
+for (i in 1:length(outputdirs)) {
+  print(paste("Processing",outputdirs[i]))
+  #gdx file
+  gdx<-path(outputdirs[i],"fulldata.gdx")
+  if(file.exists(gdx)) {
+    #get scenario name
+    load(path(outputdirs[i],"config.Rdata"))
+    scen <- cfg$title
+    
+    map_cell_clim <- readGDX(gdx,"p58_mapping_cell_climate")
+    
+    #area_pman_clim_detail
+    a <- readGDX(gdx,"ov58_peatland_man",select=list(type="level"))
+    a <- add_dimension(a,dim = 3.1,add = "scenario",nm = scen)
+    a <- dimSums(a*map_cell_clim,dim=1)
+    x$area_pman_clim_detail <- mbind(x$area_pman_clim_detail,a)
+
+    #area_p_cell
+    a <- readGDX(gdx,"ov58_peatland_man",select=list(type="level"))
+    a <- dimSums(a,dim=c(3.2))
+    a[,,"degrad"] <- a[,,"degrad"] + collapseNames(a[,,"unused"])
+    a <- a[,,"unused",invert=TRUE]
+    # miss <- readGDX(gdx,"ov58_peatland_missing",select=list(type="level"))
+    # a[,,"degrad"] <- a[,,"degrad"]+miss
+    b <- readGDX(gdx,"ov58_peatland_intact",select=list(type="level"))
+    getNames(b) <- "intact"
+    a <- mbind(a,b)
+    
+    #interpolate
+    p <- a[,getYears(a,as.integer = T)>=2015,]
+    p_ini_lr <- setYears(p[,1,],NULL)
+    p_ini_hr <- read.magpie("/p/projects/landuse/users/florianh/data/PeatArea_0.5.mz")
+    peat_hr <- interpolate(p,p_ini_lr,p_ini_hr,spam = path(outputdirs[i],"0.5-to-c200_sum.spam"))
+    peat_hr <- add_dimension(peat_hr,dim = 3.1,add = "scenario",nm = scen)
+    x$area_p_map <- mbind(x$area_p_map,peat_hr)
+    
+    peat_hr_ratio <- collapseNames(peat_hr[,,"degrad"])/dimSums(peat_hr,dim=3.2)
+    x$area_p_map_ratio <- mbind(x$area_p_map_ratio,peat_hr_ratio)
+    
+    
+    a <- add_dimension(a,dim = 3.1,add = "scenario",nm = scen)
+    x$area_p_cell <- mbind(x$area_p_cell,a)
+    
+    
+    #area_p_clim
+    a <- dimSums(a*map_cell_clim,dim=1)
+    x$area_p_clim <- mbind(x$area_p_clim,a)
+    
+    #read land
+    a <- land(gdx,level="cell")
+    bio <- setNames(croparea(gdx,level="cell",products = c("betr","begr"),product_aggr = TRUE),"bio")
+    a <- mbind(bio,a)
+    a[,,"crop"] <- a[,,"crop"]-setNames(a[,,"bio"],"crop")
+    a <- dimSums(a*map_cell_clim,dim=1)
+    a <- add_dimension(a,dim = 3.1,add = "scenario",nm = scen)
+    x$land_clim <- mbind(x$land_clim,a)
+
+    #emis_p_glo
+    emis_p_glo <- readGDX(gdx,"ov58_peatland_emis",select=list(type="level"))
+    emis_p_glo <- dimSums(emis_p_glo,dim=1)
+    emis_p_glo <- add_dimension(emis_p_glo,dim = 3.1,add = "scenario",nm = scen)
+    x$emis_p_glo <- mbind(x$emis_p_glo,emis_p_glo)
+
+    emis_p_glo[,,"ch4"] <- emis_p_glo[,,"ch4"]/28
+    emis_p_glo[,,"n2o"] <- emis_p_glo[,,"n2o"]/265
+    
+    #emis_lu_glo
+    co2 <- setNames(emisCO2(gdx,level = "glo",unit="gas",cc = TRUE),"co2")
+    doc <- setNames(co2,"doc")
+    doc[,,] <- NA
+    ch4 <- setNames(Emissions(gdx,level="glo",type="ch4",unit="gas"),"ch4")
+    n2o <- setNames(Emissions(gdx,level="glo",type="n2o_n",unit="gas"),"n2o")
+    emis_lu_glo <- mbind(co2,doc,ch4,n2o)
+    emis_lu_glo <- add_dimension(emis_lu_glo,dim = 3.1,add = "scenario",nm = scen)
+
+    lu <- add_dimension(emis_lu_glo,dim = 3.2,add = "GHG emission","Land-use change & Agriculture")
+    peat <- add_dimension(dimSums(emis_p_glo,dim=c(3.2)),dim = 3.2,add = "GHG emission","Peatland")
+    a <- mbind(lu,peat)
+    x$emis_all_glo_annual <- mbind(x$emis_all_glo_annual,a)
+    
+    #emis_p_cell_co2
+    emis_p_clim <- readGDX(gdx,"ov58_peatland_emis",select=list(type="level"))
+    emis_p_clim <- dimSums(emis_p_clim[,,c("co2","doc")],dim=c(1,3.2))
+    emis_p_clim <- add_dimension(emis_p_clim,dim = 3.1,add = "scenario",nm = scen)
+    
+    #emis_co2_clim_annual
+    emis_lu_clim <- emisCO2(gdx,level = "cell",unit="gas",cc = TRUE)
+    emis_lu_clim <- dimSums(emis_lu_clim*map_cell_clim,dim=1)
+    emis_lu_clim <- add_dimension(emis_lu_clim,dim = 3.1,add = "scenario",nm = scen)
+#    names(dimnames(emis_lu_clim)) <- names(dimnames(emis_p_clim))
+    
+    lu <- add_dimension(emis_lu_clim,dim = 3.2,add = "GHG emission","Land-use change")
+    peat <- add_dimension(emis_p_clim,dim = 3.2,add = "GHG emission","Peatland")
+    a <- mbind(lu,peat)
+    x$emis_co2_clim_annual <- mbind(x$emis_co2_clim_annual,a)
+    
+    #read fprice
+    a <- priceIndex(gdx,level="glo", products="kfo", baseyear = "y2015")
+    a <- add_dimension(a,dim = 3.1,add = "scenario",nm = scen)
+    x$fprice <- mbind(x$fprice,a)
+
+    #read food exp share
+    a <- FoodExpenditureShare(gdx, level = "glo")
+    a <- add_dimension(a,dim = 3.1,add = "scenario",nm = scen)
+    x$fexpshare <- mbind(x$fexpshare,a)
+    
+    #read kcal
+    a <- Kcal(gdx, level = "glo")
+    a <- add_dimension(a,dim = 3.1,add = "scenario",nm = scen)
+    x$kcal <- mbind(x$kcal,a)
+    
+    #read tau
+    a <- tau(gdx,level="glo")
+    a <- add_dimension(a,dim = 3.1,add = "scenario",nm = scen)
+    x$tau <- mbind(x$tau,a)
+            
+  } else missing <- c(missing,outputdirs[i])
+}
+
+#remove 1995
+x <- lapply(x, function(x) {x[,getYears(x,as.integer = T)>=2015,]})
+
+#read emis factors (same for all scenarios)
+x$emis_fac <- readGDX(gdx,"f58_ipcc_wetland_ef")
+x$map_cell_clim <- readGDX(gdx,"p58_mapping_cell_climate")
+
+x$emis_co2_clim_cum <- calcEmisCum(x$emis_co2_clim_annual)
+
+x$area_p_clim_change <- x$area_p_clim-setYears(x$area_p_clim[,1,],NULL)
+
+write.magpie(x$area_p_map_ratio,"output/map_degrad_ratio.nc",comment = "unit: Degradation Ratio")
+
+x$area_p_map_ratio_change <- x$area_p_map_ratio-setYears(x$area_p_map_ratio[,1,],NULL)
+write.magpie(x$area_p_map_ratio_change,"output/map_degrad_ratio_change.nc",comment = "unit: Change of Degradation Ratio")
+
+x$land_clim_change <- x$land_clim-setYears(x$land_clim[,1,],NULL)
+
+files <- list.files(path="output", pattern="peatland_*")
+nums <- as.numeric(gsub(paste("peatland_", ".RData", sep="|"), "", files))
+if(length(nums)==0) last=0 else last <- max(nums)
+newFile <- paste0("output/peatland_", sprintf("%02d", last + 1), ".RData")
+save(x,file = newFile,compress = "xz")
+
+if (!is.null(missing)) {
+  cat("\nList of folders with missing fulldata.gdx\n")
+  print(missing)
+}
---
+++ scripts/start/peatland.R
@@ -0,0 +1,125 @@
+# |  (C) 2008-2018 Potsdam Institute for Climate Impact Research (PIK),
+# |  authors, and contributors see AUTHORS file
+# |  This file is part of MAgPIE and licensed under GNU AGPL Version 3
+# |  or later. See LICENSE file or go to http://www.gnu.org/licenses/
+# |  Contact: magpie@pik-potsdam.de
+
+
+######################################
+#### Script to start a MAgPIE run ####
+######################################
+
+library(lucode)
+library(magclass)
+library(gdx)
+library(luscale)
+
+# Load start_run(cfg) function which is needed to start MAgPIE runs
+source("scripts/start_functions.R")
+
+getInput <- function(gdx,ghg_price=TRUE,biodem=TRUE) {
+  if(ghg_price) {
+    a <- readGDX(gdx,"f56_pollutant_prices_coupling")
+    write.magpie(a,"modules/56_ghg_policy/input/f56_pollutant_prices_coupling.cs3")
+  }
+  if(biodem) {
+    a <- readGDX(gdx,"f60_bioenergy_dem_coupling")
+    write.magpie(a,"modules/60_bioenergy/input/reg.2ndgen_bioenergy_demand.csv")
+  }
+}
+
+
+#start MAgPIE run
+source("config/default.cfg")
+cfg$results_folder <- "output/:title:"
+
+cfg$force_download <- TRUE
+
+# cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp8p5-co2_rev34_c200_690d3718e151be1b450b394c1064b1c5.tgz",
+#                "rev4.14_690d3718e151be1b450b394c1064b1c5_magpie.tgz",
+#                "rev4.14_690d3718e151be1b450b394c1064b1c5_validation.tgz",
+#                "additional_data_rev3.68_FH.tgz",
+#                "calibration_H12_c200_12Sep18.tgz",
+#                "peatland_input_v1.tgz")
+# cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"/p/projects/landuse/users/florianh/data"=NULL),
+#                            getOption("magpie_repos"))
+
+cfg$gms$peatland  <- "on"
+#cfg$gms$peatland  <- "off"
+# cfg$gms$c56_pollutant_prices <- "coupling"
+# cfg$gms$c60_2ndgen_biodem <- "coupling"
+#cfg$gms$s56_ghgprice_start <- 2020
+#cfg$gms$s56_reward_neg_emis <- -Inf
+#cfg$gms$s80_optfile <- 1
+
+prefix <- "T96"
+
+##SSP2
+cfg$title <- paste(prefix,"Ref",sep="_")
+cfg <- setScenario(cfg,c("SSP2","NPI"))
+getInput("/p/projects/remind/runs/magpie4-2019-04-02-develop/output/r8473-trunk-C_NPi-mag-4/fulldata.gdx")
+cfg$gms$c56_pollutant_prices <- "SSPDB-SSP2-Ref-REMIND-MAGPIE"
+cfg$gms$c60_2ndgen_biodem <- "SSPDB-SSP2-Ref-REMIND-MAGPIE"
+cfg$gms$s56_peatland_policy <- 0
+cfg$gms$s58_rewetting_switch  <- 0
+start_run(cfg,codeCheck=FALSE)
+
+#getInput("/p/projects/remind/runs/magpie4-2019-04-02-develop/output/r8473-trunk-C_Budg600-mag-4/fulldata.gdx")
+cfg$gms$c56_pollutant_prices <- "SSPDB-SSP2-20-REMIND-MAGPIE"
+cfg$gms$c60_2ndgen_biodem <- "SSPDB-SSP2-20-REMIND-MAGPIE"
+cfg <- setScenario(cfg,c("SSP2","NDC"))
+
+
+cfg$title <- paste(prefix,"RCP1p9",sep="_")
+cfg$gms$s56_peatland_policy <- 0
+cfg$gms$s58_rewetting_switch  <- 0
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"RCP1p9+PeatProt",sep="_")
+cfg$gms$s56_peatland_policy <- 1
+cfg$gms$s58_rewetting_switch  <- 0
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"RCP1p9+PeatRestor",sep="_")
+cfg$gms$s56_peatland_policy <- 1
+cfg$gms$s58_rewetting_switch  <- Inf
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"RCP1p9+PeatRestor_2000",sep="_")
+cfg$gms$s56_peatland_policy <- 1
+cfg$gms$s58_rewetting_switch  <- Inf
+cfg$gms$s58_rewet_cost_onetime  <- 2000
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"RCP1p9+PeatRestor_4000",sep="_")
+cfg$gms$s56_peatland_policy <- 1
+cfg$gms$s58_rewetting_switch  <- Inf
+cfg$gms$s58_rewet_cost_onetime  <- 4000
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"RCP1p9+PeatRestor_8000",sep="_")
+cfg$gms$s56_peatland_policy <- 1
+cfg$gms$s58_rewetting_switch  <- Inf
+cfg$gms$s58_rewet_cost_onetime  <- 8000
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"RCP1p9+PeatRestor_10000",sep="_")
+cfg$gms$s56_peatland_policy <- 1
+cfg$gms$s58_rewetting_switch  <- Inf
+cfg$gms$s58_rewet_cost_onetime  <- 10000
+start_run(cfg,codeCheck=FALSE)
+
+# getInput("/p/projects/remind/runs/magpie4-2019-04-02-develop/output/r8473-trunk-C_NPi-mag-4/fulldata.gdx",biodem=TRUE,ghg_price = FALSE)
+# cfg$gms$c56_emis_policy <- "none"
+# 
+# cfg$title <- paste(prefix,"Ref+PeatProt",sep="_")
+# cfg$gms$s56_peatland_policy <- 1
+# cfg$gms$s58_rewetting_switch  <- 0
+# cfg$gms$s56_aff_policy <- 0
+# start_run(cfg,codeCheck=FALSE)
+# 
+# cfg$title <- paste(prefix,"Ref+PeatRestor",sep="_")
+# cfg$gms$s56_peatland_policy <- 1
+# cfg$gms$s58_rewetting_switch  <- Inf
+# cfg$gms$s56_aff_policy <- 0
+# start_run(cfg,codeCheck=FALSE)
