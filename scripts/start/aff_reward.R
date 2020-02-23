0dbe791a2c003f76a5f13b46a0bd8aa52fce2f32
florianh <humpenoeder@pik-potsdam.de>
2020-02-23 08:41:13 +0100

    update start script

--- scripts/start/aff_reward.R
+++ scripts/start/aff_reward.R
@@ -29,11 +29,11 @@ getInput <- function(gdx,ghg_price=TRUE,biodem=TRUE) {
 }
 
 reset <- function(cfg) {
-  interest_rate(0.05)
+#  interest_rate(0.05)
   cfg$gms$s56_payment <- 0
   cfg$gms$s56_c_price_exp_aff <- 50 
   cfg$gms$s32_planing_horizon <- 50
-  cfg$gms$s52_forestry_plantation <- 0
+#  cfg$gms$s52_forestry_plantation <- 0
   cfg$gms$s56_buffer_aff <- 0.2
   return(cfg)
 }
@@ -55,87 +55,85 @@ cfg$results_folder <- "output/:title:"
 #09 with high bioen_dem
 #10 no bioen dem
 
-prefix <- "rew14"
+prefix <- "rew15"
 
-#for (ssp in c("SSP1","SSP2","SSP3","SSP4","SSP5")) {
-for (ssp in c("SSP2")) {
+for (ssp in c("SSP1","SSP2","SSP3","SSP4","SSP5")) {
   for (co2_price_path in c("Hotelling","PeakBudget")) {
+    for (forest_type in c(0,1)) {
+      for (bioen in c("BioOn","BioOff")) {
     
     cfg <- setScenario(cfg,c(ssp,"NDC"))
     cfg$gms$c56_pollutant_prices <- "coupling"
     cfg$gms$c60_2ndgen_biodem <- "coupling"
     cfg$gms$c32_aff_policy <- "none"
-    cfg$gms$interest_rate <- "glo_jan16"
-    cfg$gms$c12_interest_rate <- "coupling"
+    # cfg$gms$interest_rate <- "glo_jan16"
+    # cfg$gms$c12_interest_rate <- "coupling"
     cfg$gms$land <- "feb15"
     cfg$gms$s15_elastic_demand <- 0
     cfg$gms$c60_biodem_level <- 0
     cfg$gms$s32_recur_cost <- 1
+    cfg$gms$s52_forestry_plantation <- forest_type
     
+    if (forest_type==0) ftype="Natveg" else if (forest_type==1) ftype="Plantation"
+
     #  for (co2_price_scen in c("1p4C","1p5C","1p6C")) {
     #  file.copy(from = paste0("input/input_bioen_dem_",co2_price_path,".csv"), to = "modules/60_bioenergy/input/reg.2ndgen_bioenergy_demand.csv",overwrite = TRUE)
     file.copy(from = paste0("input/input_ghg_price_",co2_price_path,".cs3"), to = "modules/56_ghg_policy/input/f56_pollutant_prices_coupling.cs3",overwrite = TRUE)
-    file.copy(from = paste0("input/input_bioen_dem_100EJ.csv"), to = "modules/60_bioenergy/input/reg.2ndgen_bioenergy_demand.csv",overwrite = TRUE)
+    if (bioen == "BioOn") {
+      file.copy(from = paste0("input/input_bioen_dem_100EJ.csv"), to = "modules/60_bioenergy/input/reg.2ndgen_bioenergy_demand.csv",overwrite = TRUE)
+    } else if (bioen == "BioOff") {
+      file.copy(from = paste0("input/input_bioen_dem_zero.csv"), to = "modules/60_bioenergy/input/reg.2ndgen_bioenergy_demand.csv",overwrite = TRUE)
+    }
     
     cfg <- reset(cfg)
-    cfg$title <- paste0(prefix,co2_price_path,"_",ssp,"_FeedSSP5")
-    cfg$gms$c70_feed_scen <- "ssp5"
+    cfg$title <- paste0(prefix,co2_price_path,"_",ftype,"_",bioen,"_default")
     start_run(cfg,codeCheck=FALSE)
 
     cfg <- reset(cfg)
-    cfg$title <- paste0(prefix,co2_price_path,"_",ssp,"_DefFeed")
-    cfg$gms$c70_feed_scen <- "ssp2"
-    start_run(cfg,codeCheck=FALSE)
-    
-    # cfg <- reset(cfg)
-    # cfg$title <- paste0(prefix,co2_price_path,"_default")
-    # start_run(cfg,codeCheck=FALSE)
-    # 
-    # cfg <- reset(cfg)
-    # for (time_horizon in c(20,50,100)) {
-    #   cfg$title <- paste0(prefix,co2_price_path,"_",co2_price_scen,"_timehorizon_",time_horizon,"yrs")
-    #   cfg$gms$s32_planing_horizon <- time_horizon
-    #   start_run(cfg,codeCheck=FALSE)
-    # }
-    # 
+    for (time_horizon in c(20,100)) {
+      cfg$title <- paste0(prefix,co2_price_path,"_",ftype,"_",bioen,"_timehorizon_",time_horizon,"yrs")
+      cfg$gms$s32_planing_horizon <- time_horizon
+      start_run(cfg,codeCheck=FALSE)
+    }
+
     # cfg <- reset(cfg)
     # for (discount in c(0.03,0.05,0.07)) {
-    #   cfg$title <- paste0(prefix,co2_price_path,"_",co2_price_scen,"_discountrate_",discount*100)
+    #   cfg$title <- paste0(prefix,co2_price_path,"_",ftype,"_discountrate_",discount*100)
     #   interest_rate(discount)
     #   start_run(cfg,codeCheck=FALSE)
     # }
-    # 
-    # cfg <- reset(cfg)
-    # for (payment in c(0,2)) {
-    #   if (payment==0) name="annual" else if (payment==1) name="begin" else if (payment==2) name="end" else if (payment==3) name="buffer"
-    #   cfg$title <- paste0(prefix,co2_price_path,"_",co2_price_scen,"_payment_",name)
-    #   cfg$gms$s56_payment <- payment
-    #   start_run(cfg,codeCheck=FALSE)
-    # }
-    # 
-    # cfg <- reset(cfg)
-    # for (co2_price_exp in c(0,1)) {
-    #   if (co2_price_exp==0) name="myopic" else if (co2_price_exp==1) name="foresight"
-    #   cfg$title <- paste0(prefix,co2_price_path,"_",co2_price_scen,"_co2priceexp_",name)
-    #   cfg$gms$s56_c_price_exp_aff <- co2_price_exp*cfg$gms$s32_planing_horizon
-    #   start_run(cfg,codeCheck=FALSE)
-    # }
-    # 
+
+    cfg <- reset(cfg)
+    for (payment in c(2)) {
+      if (payment==0) name="annual" else if (payment==1) name="begin" else if (payment==2) name="end" else if (payment==3) name="buffer"
+      cfg$title <- paste0(prefix,co2_price_path,"_",ftype,"_",bioen,"_payment_",name)
+      cfg$gms$s56_payment <- payment
+      start_run(cfg,codeCheck=FALSE)
+    }
+
+    cfg <- reset(cfg)
+    for (co2_price_exp in c(0)) {
+      if (co2_price_exp==0) name="myopic" else if (co2_price_exp==1) name="foresight"
+      cfg$title <- paste0(prefix,co2_price_path,"_",ftype,"_",bioen,"_co2priceexp_",name)
+      cfg$gms$s56_c_price_exp_aff <- co2_price_exp*cfg$gms$s32_planing_horizon
+      start_run(cfg,codeCheck=FALSE)
+    }
+
     # cfg <- reset(cfg)
     # for (forest_type in c(0,1)) {
     #   if (forest_type==0) name="natveg" else if (forest_type==1) name="plantation"
-    #   cfg$title <- paste0(prefix,co2_price_path,"_",co2_price_scen,"_foresttype_",name)
+    #   cfg$title <- paste0(prefix,co2_price_path,"_",ftype,"_foresttype_",name)
     #   cfg$gms$s52_forestry_plantation <- forest_type
     #   start_run(cfg,codeCheck=FALSE)
     # }
-    # 
-    # cfg <- reset(cfg)
-    # for (buffer in c(0,0.2,0.4)) {
-    #   cfg$title <- paste0(prefix,co2_price_path,"_",co2_price_scen,"_buffer_",buffer*100)
-    #   cfg$gms$s56_buffer_aff <- buffer
-    #   start_run(cfg,codeCheck=FALSE)
-    # }
-    # }
-  
+
+    cfg <- reset(cfg)
+    for (buffer in c(0,0.4)) {
+      cfg$title <- paste0(prefix,co2_price_path,"_",ftype,"_",bioen,"_buffer_",buffer*100)
+      cfg$gms$s56_buffer_aff <- buffer
+      start_run(cfg,codeCheck=FALSE)
+    }
+    }
+    }
   }
 }
