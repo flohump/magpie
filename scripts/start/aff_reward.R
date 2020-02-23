0ef3fb445ad1a1bd37194280d3f4bc9f816d290c
florianh <humpenoeder@pik-potsdam.de>
2020-02-23 08:52:18 +0100

    update

--- scripts/start/aff_reward.R
+++ scripts/start/aff_reward.R
@@ -55,7 +55,7 @@ cfg$results_folder <- "output/:title:"
 #09 with high bioen_dem
 #10 no bioen dem
 
-prefix <- "rew15"
+prefix <- "rew15_"
 
 for (ssp in c("SSP1","SSP2","SSP3","SSP4","SSP5")) {
   for (co2_price_path in c("Hotelling","PeakBudget")) {
