84f74f68901509ab90af1959f5e7e9917899e20a
florianh <humpenoeder@pik-potsdam.de>
2020-03-01 09:40:53 +0100

    update

--- scripts/start/ssp2.R
+++ scripts/start/ssp2.R
@@ -19,22 +19,23 @@ source("scripts/start_functions.R")
 #start MAgPIE run
 source("config/default.cfg")
 
-
+prefix <- "res02"
 
 cfg$results_folder <- "output/:title:"
 
-for (res in c("c600","c1000","c10000")) {
-  for (opt in c("nlp_apr17","nlp_par")) {
+#for (res in c("c600","c1000","c10000")) {
+for (res in c("c200")) {
+  for (opt in c("nlp_apr17")) {#"nlp_par"
     cfg$input <- c(paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev42_",res,"_690d3718e151be1b450b394c1064b1c5.tgz"),
                    "rev4.42_690d3718e151be1b450b394c1064b1c5_magpie.tgz",
                    "rev4.42_690d3718e151be1b450b394c1064b1c5_validation.tgz",
                    "calibration_H12_c200_26Feb20.tgz",
                    "additional_data_rev3.77.tgz")
     
-    cfg$title <- paste0("res_SSP2_",res,"_",opt)
+    cfg$title <- paste0(prefix,"_SSP2_",res,"_",opt)
     cfg <- setScenario(cfg,c("SSP2","NPI"))
     cfg$gms$optimization <- opt
-    cfg$gms$trade <- "off"
+    #cfg$gms$trade <- "off"
     cfg$force_download <- TRUE
     cfg$recalc_npi_ndc <- TRUE
     start_run(cfg,codeCheck=FALSE)
