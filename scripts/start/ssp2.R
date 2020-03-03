a05f26a1aeb21feccb007a7f3cd1eb7a0f6b840e
florianh <humpenoeder@pik-potsdam.de>
2020-03-03 14:52:50 +0100

    udpate

--- scripts/start/ssp2.R
+++ scripts/start/ssp2.R
@@ -19,12 +19,12 @@ source("scripts/start_functions.R")
 #start MAgPIE run
 source("config/default.cfg")
 
-prefix <- "res12"
+prefix <- "res13"
 
 cfg$results_folder <- "output/:title:"
 
-for (res in c("c200","c600","c1000")) {#"c1000","c10000"
-#for (res in c("c200")) {
+#for (res in c("c200","c600","c1000")) {#"c1000","c10000"
+for (res in c("c200")) {
   for (opt in c("nlp_par")) {#"nlp_par"
     cfg$input <- c(paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev42_",res,"_690d3718e151be1b450b394c1064b1c5.tgz"),
                    "rev4.42_690d3718e151be1b450b394c1064b1c5_magpie.tgz",
