d098dbd40516b878a290de5ac370a33881dae7c6
florianh <humpenoeder@pik-potsdam.de>
2020-03-29 11:46:47 +0200

    output script

---
+++ scripts/output/comparison/aff_area_spatial.R
@@ -0,0 +1,55 @@
+# |  (C) 2008-2019 Potsdam Institute for Climate Impact Research (PIK)
+# |  authors, and contributors see CITATION.cff file. This file is part
+# |  of MAgPIE and licensed under AGPL-3.0-or-later. Under Section 7 of
+# |  AGPL-3.0, you are granted additional permissions described in the
+# |  MAgPIE License Exception, version 1.0 (see LICENSE file).
+# |  Contact: magpie@pik-potsdam.de
+
+#########################
+#### check modelstat ####
+#########################
+# Version 1.0, Florian Humpenoeder
+#
+library(lucode)
+library(magclass)
+library(luplot)
+library(magpie4)
+library(ggplot2)
+library(data.table)
+library(quitte)
+
+options(error=function()traceback(2))
+
+############################# BASIC CONFIGURATION #############################
+if(!exists("source_include")) {
+  outputdirs <- path("output/",list.dirs("output/", full.names = FALSE, recursive = FALSE))
+  #Define arguments that can be read from command line
+  readArgs("outputdirs")
+}
+###############################################################################
+cat("\nStarting output generation\n")
+
+forestry <- NULL
+missing <- NULL
+
+for (i in 1:length(outputdirs)) {
+  print(paste("Processing",outputdirs[i]))
+  #gdx file
+  gdx<-path(outputdirs[i],"fulldata.gdx")
+  if(file.exists(gdx)) {
+    #get scenario name
+    load(path(outputdirs[i],"config.Rdata"))
+    scen <- cfg$title
+    #read-in reporting file
+    x <- read.magpie("cell.land_0.5_share.mz")
+    x <- x-setYears(x[,2020,],NULL)
+    x <- x[,c(2050,2100),]
+    forestry <- mbind(forestry,setNames(x,scen))
+  } else missing <- c(missing,outputdirs[i])
+}
+if (!is.null(missing)) {
+  cat("\nList of folders with missing fulldata.gdx\n")
+  print(missing)
+}
+
+write.magpie(forestry,"output/aff_area_shr.mz")
