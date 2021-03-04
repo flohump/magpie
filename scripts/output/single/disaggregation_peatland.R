308fb22136bd3acbe5a2be6de4519cafab05117c
florianh <humpenoeder@pik-potsdam.de>
2021-03-04 09:51:13 +0100

    update

--- scripts/output/single/disaggregation_peatland.R
+++ scripts/output/single/disaggregation_peatland.R
@@ -168,15 +168,10 @@ getNames(tech) <- c("Emissions|Peatland|CH4 (Mt CO2eq/yr)","Emissions|Peatland|N
 y <- dimSums(tech,dim=3)
 getNames(y) <- "Emissions|Peatland|Total (Mt CO2eq/yr)"
 tech <- mbind(tech,y)
-#add cumulative emissions
+#cumulative emissions not possible in this case
 a <- y
-im_years <- new.magpie("GLO",getYears(a),NULL)
-im_years[,,] <- c(1,diff(getYears(a,as.integer = TRUE)))
-a[,"y1995",] <- 0
-a <- a*im_years[,getYears(a),]
-a <- as.magpie(apply(a,c(1,3),cumsum))
-a <- a - setYears(a[,"y2015",],NULL)
 getNames(a) <- "Emissions|Peatland|Total|Cumulative (Mt CO2eq since 2015)"
+a[,,] <- NA
 tech <- mbind(tech,a)
 
 #area
