a3bb6cb6bf1744a2c2e38c011be13a519a5b7c2f
florianh <humpenoeder@pik-potsdam.de>
2021-06-21 22:41:00 +0200

    test

--- scripts/start/projects/test_LUSPA.R
+++ scripts/start/projects/test_LUSPA.R
@@ -41,11 +41,12 @@ cfg$gms$s58_rewetting_switch  <- Inf
 cfg$output <- c("rds_report")
 #download_and_update(cfg)
 
-prefix <- "INF02"
+prefix <- "INF03"
 
 
 source("scripts/start/extra/lpjml_addon.R")
-cfg$gms$land <- "feb15"
+#cfg$gms$land <- "feb15"
+cfg$gms$crop    <- "endo_jun13"
 cfg$results_folder <- "output/:title:"
 cfg$gms$s58_rewetting_switch  <- Inf
 #cfg$gms$s80_optfile <- 1
