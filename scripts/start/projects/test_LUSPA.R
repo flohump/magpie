204355d6f52aef1dc974a20000d21d73b3bee27d
florianh <humpenoeder@pik-potsdam.de>
2021-06-22 08:05:27 +0200

    test

--- scripts/start/projects/test_LUSPA.R
+++ scripts/start/projects/test_LUSPA.R
@@ -41,12 +41,12 @@ cfg$gms$s58_rewetting_switch  <- Inf
 cfg$output <- c("rds_report")
 #download_and_update(cfg)
 
-prefix <- "INF05"
+prefix <- "INF06"
 
 
 source("scripts/start/extra/lpjml_addon.R")
-cfg$gms$c52_carbon_scenario  <- "nocc"
-cfg$gms$c59_som_scenario  <- "nocc"
+#cfg$gms$c52_carbon_scenario  <- "nocc"
+#cfg$gms$c59_som_scenario  <- "nocc"
 #cfg$gms$land <- "feb15"
 #cfg$gms$crop    <- "endo_jun13"
 cfg$results_folder <- "output/:title:"
@@ -91,6 +91,6 @@ cfg$output <- c("rds_report")
 
 cfg$title <- paste(prefix,"SSP5","1p5deg","SPA5",sep="_")
 cfg <- setScenario(cfg,c("SSP5","NDC"))
-cfg$gms$c56_pollutant_prices <- "R21M42-SSP5-PkBudg900"
-cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP5-PkBudg900"
+cfg$gms$c56_pollutant_prices <- "R21M42-SSP2-PkBudg900"
+cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP2-PkBudg900"
 start_run(cfg,codeCheck=FALSE)
