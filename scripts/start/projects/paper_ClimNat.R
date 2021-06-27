9d7213f71ab5d2e02ac799c0f77d7cf99ab10656
florianh <humpenoeder@pik-potsdam.de>
2021-06-27 07:36:03 +0200

    update

--- scripts/start/projects/paper_ClimNat.R
+++ scripts/start/projects/paper_ClimNat.R
@@ -23,7 +23,7 @@ source("scripts/start_functions.R")
 
 #start MAgPIE run
 source("config/default.cfg")
-source("scripts/start/extra/lpjml_addon.R")
+#source("scripts/start/extra/lpjml_addon.R")
 #cfg$gms$c52_carbon_scenario  <- "nocc"
 #cfg$gms$c59_som_scenario  <- "nocc"
 
@@ -32,7 +32,7 @@ source("scripts/start/extra/lpjml_addon.R")
 cfg$results_folder <- "output/:title:"
 cfg$output <- c("rds_report","extra/disaggregation")
 
-prefix <- "VE26"
+prefix <- "VE27"
 cfg$qos <- "priority"
 
 cfg$gms$s80_optfile <- 1
@@ -48,8 +48,8 @@ for (pol in c("Ref","Climate","Nature","Climate+Nature")) {
   for (ssp in c("SSP2")) {
     if (pol == "Ref") {
       cfg <- setScenario(cfg,c(ssp,"NDC"))
-      cfg$gms$c56_pollutant_prices <- "R21M42-SSP2-NPi"
-      cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP2-NPi"
+      cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NDC" #"R21M42-SSP2-NPi"
+      cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-NDC" #"R21M42-SSP2-NPi"
       cfg$gms$s32_aff_plantation <- 0
       cfg$gms$s32_aff_bii_coeff <- 0
       cfg$gms$c44_price_bv_loss <- "p0"
@@ -58,8 +58,8 @@ for (pol in c("Ref","Climate","Nature","Climate+Nature")) {
       cfg$gms$s30_set_aside_shr <- 0
     } else if (pol == "Climate") {
       cfg <- setScenario(cfg,c(ssp,"NDC"))
-      cfg$gms$c56_pollutant_prices <- "R21M42-SSP2-PkBudg900"
-      cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP2-PkBudg900"
+      cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg600" #"R21M42-SSP2-PkBudg900"
+      cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-Budg600" #"R21M42-SSP2-PkBudg900"
       cfg$gms$s32_aff_plantation <- 1
       cfg$gms$s32_aff_bii_coeff <- 1
       cfg$gms$c44_price_bv_loss <- "p0"
@@ -68,21 +68,21 @@ for (pol in c("Ref","Climate","Nature","Climate+Nature")) {
       cfg$gms$s30_set_aside_shr <- 0
     } else if (pol == "Nature") {
       cfg <- setScenario(cfg,c(ssp,"NDC"))
-      cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
-      cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-NPi"
+      cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NDC" #"R2M41-SSP2-NPi"
+      cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-NDC" #"R2M41-SSP2-NPi"
       cfg$gms$s32_aff_plantation <- 0
       cfg$gms$s32_aff_bii_coeff <- 0
-      cfg$gms$c44_price_bv_loss <- "p100_p5000_by2050" #"p10_p10000"
+      cfg$gms$c44_price_bv_loss <- "p3000" #"p10_p10000"
       cfg$gms$c35_protect_scenario <- "FF_BH"
       cfg$gms$c30_set_aside_target <- "by2030"
       cfg$gms$s30_set_aside_shr <- 0.2
     } else if (pol == "Climate+Nature") {
       cfg <- setScenario(cfg,c(ssp,"NDC"))
-      cfg$gms$c56_pollutant_prices <- "R21M42-SSP2-PkBudg900"
-      cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP2-NPi"
+      cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg600" #"R21M42-SSP2-PkBudg900"
+      cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-NDC" #"R21M42-SSP2-NPi"
       cfg$gms$s32_aff_plantation <- 0
       cfg$gms$s32_aff_bii_coeff <- 0
-      cfg$gms$c44_price_bv_loss <- "p100_p5000_by2050" #"p10_p10000"
+      cfg$gms$c44_price_bv_loss <- "p3000" #"p10_p10000"
       cfg$gms$c35_protect_scenario <- "FF_BH"
       cfg$gms$c30_set_aside_target <- "by2030"
       cfg$gms$s30_set_aside_shr <- 0.2
