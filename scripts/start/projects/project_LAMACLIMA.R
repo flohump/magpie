a019d753d020d4deb21b94296d89ad0b95d6a607
florianh <humpenoeder@pik-potsdam.de>
2021-06-30 06:41:35 +0200

    update

--- scripts/start/projects/project_LAMACLIMA.R
+++ scripts/start/projects/project_LAMACLIMA.R
@@ -45,8 +45,9 @@ cfg$output <- c("rds_report","extra/disaggregation_transitions")
 #29: labour prod*0.5
 #30: labour prod*1
 #31 labour prod*1 + sticky_labour_jul21
+#32 labour prod*0.5 + sticky_labour_jul21
 
-prefix <- "LAMA31"
+prefix <- "LAMA32"
 cfg$force_replace <- TRUE
 
 cfg$gms$s80_optfile <- 1
