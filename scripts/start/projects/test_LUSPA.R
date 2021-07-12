e70e2de02c8ccd8a73519d65808f35f1da184300
florianh <humpenoeder@pik-potsdam.de>
2021-07-12 21:45:24 +0200

    test

--- scripts/start/projects/test_LUSPA.R
+++ scripts/start/projects/test_LUSPA.R
@@ -39,61 +39,61 @@ cfg$results_folder <- "output/:title:"
 cfg$output <- c("rds_report")
 #download_and_update(cfg)
 
-prefix <- "LU06"
+prefix <- "LU07"
 
-# #SSP1
-# cfg$title <- paste(prefix,"SSP1","REF","SPA0",sep="_")
-# cfg <- setScenario(cfg,c("SSP1","NPI"))
-# cfg$gms$c56_pollutant_prices <- "R21M42-SSP1-NPi"
-# cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP1-NPi"
-# start_run(cfg,codeCheck=FALSE)
-# 
-# cfg$title <- paste(prefix,"SSP1","1p5deg","SPA1",sep="_")
-# cfg <- setScenario(cfg,c("SSP1","NDC"))
-# cfg$gms$c56_pollutant_prices <- "R21M42-SSP1-PkBudg900"
-# cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP1-PkBudg900"
-# start_run(cfg,codeCheck=FALSE)
-# 
-# #SSP2
-# cfg$title <- paste(prefix,"SSP2","REF","SPA0",sep="_")
-# cfg <- setScenario(cfg,c("SSP2","NPI"))
-# cfg$gms$c56_pollutant_prices <- "R21M42-SSP2-NPi"
-# cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP2-NPi"
-# start_run(cfg,codeCheck=FALSE)
-# 
-# cfg$title <- paste(prefix,"SSP2","1p5deg","SPA2",sep="_")
-# cfg <- setScenario(cfg,c("SSP2","NDC"))
-# cfg$gms$c56_pollutant_prices <- "R21M42-SSP2-PkBudg900"
-# cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP2-PkBudg900"
-# start_run(cfg,codeCheck=FALSE)
-# 
-# cfg$title <- paste(prefix,"SSP2","1p5deg","SPA0",sep="_")
-# cfg <- setScenario(cfg,c("SSP2","NDC"))
-# cfg$gms$c56_lu_spa <- "none"
-# cfg$gms$c56_pollutant_prices <- "R21M42-SSP2-PkBudg900"
-# cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP2-PkBudg900"
-# start_run(cfg,codeCheck=FALSE)
-# 
+#SSP1
+cfg$title <- paste(prefix,"SSP1","REF","SPA0",sep="_")
+cfg <- setScenario(cfg,c("SSP1","NPI"))
+cfg$gms$c56_pollutant_prices <- "R21M42-SSP1-NPi"
+cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP1-NPi"
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"SSP1","1p5deg","SPA1",sep="_")
+cfg <- setScenario(cfg,c("SSP1","NDC"))
+cfg$gms$c56_pollutant_prices <- "R21M42-SSP1-PkBudg900"
+cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP1-PkBudg900"
+start_run(cfg,codeCheck=FALSE)
+
+#SSP2
+cfg$title <- paste(prefix,"SSP2","REF","SPA0",sep="_")
+cfg <- setScenario(cfg,c("SSP2","NPI"))
+cfg$gms$c56_pollutant_prices <- "R21M42-SSP2-NPi"
+cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP2-NPi"
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"SSP2","1p5deg","SPA2",sep="_")
+cfg <- setScenario(cfg,c("SSP2","NDC"))
+cfg$gms$c56_pollutant_prices <- "R21M42-SSP2-PkBudg900"
+cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP2-PkBudg900"
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"SSP2","1p5deg","SPA0",sep="_")
+cfg <- setScenario(cfg,c("SSP2","NDC"))
+cfg$gms$c56_lu_spa <- "none"
+cfg$gms$c56_pollutant_prices <- "R21M42-SSP2-PkBudg900"
+cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP2-PkBudg900"
+start_run(cfg,codeCheck=FALSE)
+
 #SSP5
-cfg$title <- paste(prefix,"SSP5","REF","SPA0","CHECK2",sep="_")
+cfg$title <- paste(prefix,"SSP5","REF","SPA0",sep="_")
 cfg <- setScenario(cfg,c("SSP5","NPI"))
 cfg$gms$c56_pollutant_prices <- "R21M42-SSP5-NPi"
 cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP5-NPi"
 start_run(cfg,codeCheck=FALSE)
 
-# cfg$title <- paste(prefix,"SSP5","1p5deg","SPA5",sep="_")
-# cfg <- setScenario(cfg,c("SSP5","NDC"))
-# cfg$gms$c56_pollutant_prices <- "R21M42-SSP5-PkBudg900"
-# cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP5-PkBudg900"
-# cfg$gms$c60_biodem_level <- 0 #otherwise infeasible
-# start_run(cfg,codeCheck=FALSE)
-# 
-# cfg$title <- paste(prefix,"SSP5","1p5deg","SPA0",sep="_")
-# cfg <- setScenario(cfg,c("SSP5","NDC"))
-# cfg$gms$c56_lu_spa <- "none"
-# cfg$gms$s32_aff_plantation <- 0
-# cfg$gms$s32_aff_bii_coeff <- 0
-# cfg$gms$c56_pollutant_prices <- "R21M42-SSP5-PkBudg900"
-# cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP5-PkBudg900"
-# cfg$gms$c60_biodem_level <- 0 #otherwise infeasible
-# start_run(cfg,codeCheck=FALSE)
+cfg$title <- paste(prefix,"SSP5","1p5deg","SPA5",sep="_")
+cfg <- setScenario(cfg,c("SSP5","NDC"))
+cfg$gms$c56_pollutant_prices <- "R21M42-SSP5-PkBudg900"
+cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP5-PkBudg900"
+cfg$gms$c60_biodem_level <- 0 #otherwise infeasible
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"SSP5","1p5deg","SPA0",sep="_")
+cfg <- setScenario(cfg,c("SSP5","NDC"))
+cfg$gms$c56_lu_spa <- "none"
+cfg$gms$s32_aff_plantation <- 0
+cfg$gms$s32_aff_bii_coeff <- 0
+cfg$gms$c56_pollutant_prices <- "R21M42-SSP5-PkBudg900"
+cfg$gms$c60_2ndgen_biodem <- "R21M42-SSP5-PkBudg900"
+cfg$gms$c60_biodem_level <- 0 #otherwise infeasible
+start_run(cfg,codeCheck=FALSE)
